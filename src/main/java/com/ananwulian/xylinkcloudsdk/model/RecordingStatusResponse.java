6e5d557656a718f640bd87ebe3d996d4e2208ed6
ghostsf <ghost_sf@163.com>
2020-06-22 16:20:28 +0800

    fix: RecordingStatusResponse add INVALID_RECORDING_OPERATION

--- src/main/java/com/ananwulian/xylinkcloudsdk/model/RecordingStatusResponse.java
+++ src/main/java/com/ananwulian/xylinkcloudsdk/model/RecordingStatusResponse.java
@@ -4,5 +4,5 @@ package com.ananwulian.xylinkcloudsdk.model;
  * Created by maolizhi on 4/7/17.
  */
 public enum RecordingStatusResponse {
-    NO_MEETING,SIG_SERVER_TIMEOUT, UNKNOWN_ERROR, OK,;
+    NO_MEETING,SIG_SERVER_TIMEOUT, UNKNOWN_ERROR, INVALID_RECORDING_OPERATION, OK,;
 }
