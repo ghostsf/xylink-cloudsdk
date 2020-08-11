49c79d6ceb4ef9dd23e7179a2ec3d98baf9c036c
ghostsf <ghost_sf@163.com>
2020-08-11 10:14:11 +0800

    fix: update CallbackEvent; tag: version 1.1.6

--- src/main/java/com/ananwulian/xylinkcloudsdk/model/callback/CallbackEvent.java
+++ src/main/java/com/ananwulian/xylinkcloudsdk/model/callback/CallbackEvent.java
@@ -4,7 +4,18 @@ package com.ananwulian.xylinkcloudsdk.model.callback;
  * Created by wenya on 16/4/9.
  */
 public enum CallbackEvent {
-    NemoUnbound,LiveStatus,NewCallPush,NewUserCall,ConferenceRosterInfo,
-    // 录制结束
-    RecordingStop
+    // 入会/出会（企业级别回调）
+    NewUserCall,
+    // 直播状态变更回调（企业级别回调）
+    LiveStatus,
+    // 云会议室参会人状态（云会议室级别回调）
+    ConferenceRosterInfo,
+    // 视频录制结束（企业级别回调）
+    RecordingStop,
+    // 开启录制状态反馈（如需要知道某个开启录制的动作是否成功）（企业级别回调）
+    RecordStatus,
+    // 转码状态反馈（企业级别回调）
+    TranscodingStatus,
+    // 云会议室参会人举手动作（云会议室级别回调）
+    NemoHands
 }
